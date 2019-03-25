7f65f8fe74a9261a1c4ab753e6087b519c483d49
Kenzie Tandun <nudnateiznek@gmail.com>
2019-03-26 10:09:25 +1300

    compile source files before testing

--- runtests.sh
+++ runtests.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+SOURCE_DIRS=(
+    "src/template"
+    )
+
+for JAVA_PACKAGE in "${SOURCE_DIRS[*]}"
+do
+    echo "Compiling .java files in ${JAVA_PACKAGE}"
+    javac -d bin "${JAVA_PACKAGE}/"*.java
+done
+
+echo "done"
+
+# compile all tests file
 javac -d bin -sourcepath src -cp .:lib/junit-platform-console-standalone-1.4.1.jar src/test/*.java
 
+# run all tests from package "test"
 java -jar lib/junit-platform-console-standalone-1.4.1.jar --cp bin/ -p test 
+
+# add more tests cases below
