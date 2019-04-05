8bdc9a58340cd6b654469540e2e04820483d33db
kenzie tandun <nudnateiznek@gmail.com>
2019-04-05 21:11:07 +1300

    fix indentation and remove autogenerated code

--- src/unit/Medic.java
+++ src/unit/Medic.java
@@ -2,14 +2,13 @@ package unit;
 
 public class Medic extends CrewMember {
 
-	public Medic(String memberName, int luckStat) {
-		super(memberName, 10);
-		// TODO Auto-generated constructor stub
-	}
-	
-	@Override
+    public Medic(String memberName, int luckStat) {
+        super(memberName, 10);
+    }
+
+    @Override
     public void reduceHealth(int amount) {
-		int health = super.getHealth();
+        int health = super.getHealth();
         health -= (amount / 2);
         if (health < 0) {
             health = 0;
