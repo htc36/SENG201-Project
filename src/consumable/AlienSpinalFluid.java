e170c63fb908e9bcc71f22ff9d0fc07e4b319da7
kenzie tandun <nudnateiznek@gmail.com>
2019-04-07 16:36:54 +1200

    remove space from name

--- src/consumable/AlienSpinalFluid.java
+++ src/consumable/AlienSpinalFluid.java
@@ -3,7 +3,7 @@ package consumable;
 public class AlienSpinalFluid extends MedicalSupply {
 
 	public AlienSpinalFluid() {
-		super("Alien Spinal Fluid", 10, 10, false);
+		super("AlienSpinalFluid", 10, 10, false);
 		// TODO Auto-generated constructor stub
 	}
 
