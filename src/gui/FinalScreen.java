2668d667079c0e2455476f6a4567291979c0b334
Bobby Joe <bobbyjoe2025@gmail.com>
2019-04-28 16:14:05 +1200

    added to final screen

--- src/gui/FinalScreen.java
+++ src/gui/FinalScreen.java
@@ -63,5 +63,36 @@ public class FinalScreen {
         lblFinalScoreCount.setBounds(62, 180, 324, 80);
         frame.getContentPane().add(lblFinalScoreCount);
         lblFinalScoreCount.setText(String.valueOf(engine.getFinalScore()));
+        
+        JLabel lblNewLabel = new JLabel("Ships Name:");
+        lblNewLabel.setForeground(Color.WHITE);
+        lblNewLabel.setBounds(12, 58, 158, 48);
+        frame.getContentPane().add(lblNewLabel);
+        
+        JLabel lblNewLabel_1 = new JLabel("Number of Days:");
+        lblNewLabel_1.setForeground(Color.WHITE);
+        lblNewLabel_1.setBounds(12, 105, 146, 15);
+        frame.getContentPane().add(lblNewLabel_1);
+        
+        JLabel shipPieces = new JLabel("fgf");
+        shipPieces.setForeground(Color.WHITE);
+        shipPieces.setBounds(12, 132, 390, 36);
+        if (engine.hasFoundEnoughPieces()) 
+        	shipPieces.setText("<html>Congratulations you won the game and have collected all the ship pieces</html>");
+        else
+        	shipPieces.setText("<html>Better luck next time, you did not collect all the ship pieces</html>");
+        frame.getContentPane().add(shipPieces);
+        
+        JLabel shipNameDetail = new JLabel("New label");
+        shipNameDetail.setForeground(Color.WHITE);
+        shipNameDetail.setBounds(197, 75, 205, 15);
+        shipNameDetail.setText(engine.getSpaceshipName());
+        frame.getContentPane().add(shipNameDetail);
+        
+        JLabel days = new JLabel("New label");
+        days.setForeground(Color.WHITE);
+        days.setBounds(197, 105, 66, 15);
+        days.setText(String.valueOf(engine.getCurrDay()));
+        frame.getContentPane().add(days);
     }
 }
