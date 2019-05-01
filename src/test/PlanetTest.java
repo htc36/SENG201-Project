f5ca65d0388134e37ca1e169521ff866ee9ee38d
Bobby Joe <bobbyjoe2025@gmail.com>
2019-05-01 12:28:05 +1200

    added planetTest

---
+++ src/test/PlanetTest.java
@@ -0,0 +1,35 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import planet.Planet;
+
+
+class PlanetTest {
+	private Planet p1;
+	private Planet p2;
+	private Planet p3;
+
+
+	@BeforeEach
+	public void setup() {
+		
+		p1 = new Planet("Mercury", false);
+		p2 = new Planet("Mars", true);
+		p3 = new Planet("Venus", true);
+	}
+	
+	@Test
+	public void testForShipPieces() {
+		p2.extractShipPieces();
+		p1.extractShipPieces();
+		assertEquals(false, p2.stillHasShipPieces());
+		assertEquals(false, p1.stillHasShipPieces());
+		assertEquals(true, p3.stillHasShipPieces());
+	}
+	
+	
+}
