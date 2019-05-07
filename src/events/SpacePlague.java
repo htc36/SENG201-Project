99c355d3d30516318a909443dda9b7f8b5655083
Kenzie Tandun <kta79@cs19092bs.canterbury.ac.nz>
2019-05-07 20:24:51 +1200

    better style

--- src/events/SpacePlague.java
+++ src/events/SpacePlague.java
@@ -19,12 +19,12 @@ public class SpacePlague implements RandomEvents {
         Random rand = new Random();
         int index = rand.nextInt(c.size());
         ArrayList<Integer> indexs = new ArrayList<>();
-        while (!indexs.contains(index)) {
+        do {
             indexs.add(index);
             c.get(index).makeSick();
             c.get(index).reduceHealth(10);
             index = rand.nextInt(c.size());
-        }
+        } while (!indexs.contains(index));
     }
 
 }
