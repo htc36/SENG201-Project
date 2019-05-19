ad1abc22732a5f0ad0c854986edc7c78335c3209
Kenzie Tandun <nudnateiznek@gmail.com>
2019-05-19 15:35:55 +1200

    update javadoc

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"consumable"},{"l":"crew"},{"l":"events"},{"l":"game"},{"l":"gui"},{"l":"outpost"},{"l":"planet"},{"l":"unit"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"consumable"},{"l":"crew"},{"l":"events"},{"l":"game"},{"l":"gui"},{"l":"outpost"},{"l":"planet"},{"l":"unit"}]
